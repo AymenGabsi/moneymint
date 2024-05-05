2f31014d421f9518701570d23eb5350620d1e326
Aymen Gabsi <aymengabsi20@gmail.com>
2024-05-05 14:33:58 +0200

    [MoneyMint] Set up the mobile navigation sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,32 @@
+import MobileNav from "@/components/MobileNav";
+import { Sidebar } from "lucide-react";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Aymen", lastName: "Gabsi" };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src="/icons/logo.svg"
+            height={30}
+            width={30}
+            alt="MoneyMint logo"
+          />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }

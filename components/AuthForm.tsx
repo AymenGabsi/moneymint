293fb5efb54e7fce10be00e872d5bb7be733f2f7
Aymen Gabsi <aymengabsi20@gmail.com>
2024-05-11 12:52:29 +0200

    [MoneyMint] Fix the implementation of Plaid

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,7 +21,7 @@ import CustomInput from "./CustomInput";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
-import { PlaidLink } from "react-plaid-link";
+import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();

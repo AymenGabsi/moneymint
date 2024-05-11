3cb4ed6fd444874f6e18821aa157c3fed85f6f3d
Aymen Gabsi <aymengabsi20@gmail.com>
2024-05-11 13:42:37 +0200

    [MoneyMint] Displaying Real Bank Data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  });
+
+  //! if we don't return accounts, we just exit and we don't render this page ...
+  if (!accounts) return;
+
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
+
   return (
     <section className="home">
       <div className="home-content">
@@ -12,14 +25,14 @@ const Home = async () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.name || "Guest"}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
 
           <TotalBalanceBox
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1250.35}
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
         RECENT TRANSACTIONS
@@ -27,9 +40,9 @@ const Home = async () => {
 
       <RightSidebar
         user={loggedIn}
-        transactions={[]}
+        transactions={accounts?.transactions}
         //banks={[]}
-        banks={[{ currentBalance: 1250.35 }, { currentBalance: 4550.35 }]}
+        banks={accountsData?.slice(0, 2)}
       />
     </section>
   );
